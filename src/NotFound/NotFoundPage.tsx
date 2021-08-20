ff26070285da3719cf6fe91c2bff21515b509e79
Tdrj2716 <ri76joy@gmail.com>
2021-08-21 00:34:20 +0900

    add 404 page

---
+++ src/NotFound/NotFoundPage.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Box, Typography } from "@material-ui/core";
+import beachImage from "./monochrome-beach.jpeg";
+import styled from "styled-components";
+
+const homeUrl = "https://tdrj2716.github.io";
+
+const NotFoundPage: React.FC = () => {
+	const handleClick = () => {
+		window.location.href = homeUrl;
+	};
+	return (
+		<StyledBox display="flex" flexDirection="column" justifyContent="center">
+			<Typography variant="h1" align="center" color="inherit">
+				404 Error
+			</Typography>
+			<Typography variant="h6" align="center" color="inherit">
+				Sorry, but we can't find what you are looking for.
+			</Typography>
+			<StyledButton onClick={handleClick}>See top page</StyledButton>
+		</StyledBox>
+	);
+};
+
+const StyledButton = styled.button`
+	align-self: center;
+	margin-top: 70px;
+	border: none;
+	size: 240px 100px;
+	background: transparent;
+	font-size: 36px;
+	opacity: 0.8;
+	transition: opacity 0.2s;
+
+	:hover {
+		opacity: 0.5;
+		transition: opacity 0.2s;
+	}
+`;
+
+const StyledBox = styled(Box)`
+	width: 100%;
+	height: 100vh;
+	background-image: url(${beachImage});
+	background-size: cover;
+	background-position: center;
+`;
+
+export default NotFoundPage;
